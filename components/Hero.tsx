2556b0fa3177610f69f5dabe13429cbb4ccd94fe
DaniPhilo <daniel.carrasco.casado@gmail.com>
2023-08-17 15:03:14 +0200

    Add syntax highlighter

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,11 @@
+"use client"
+import Link from 'next/link';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
 const Hero = () => {
+
+    const customClass = "bg-background-fill"
     return (
         <div className="flex flex-col justify-center items-start gap-14">
             <div>
@@ -9,7 +16,17 @@ const Hero = () => {
             <div className="flex flex-col justify-center items-start gap-2">
                 <p>// complete the game to continue</p>
                 <p>// you can also see it on my Github page</p>
-                <p>const githubLink = “https://github.com/example/url”</p>
+                <Link href={"https://github.com/DaniPhilo"} className='hover:underline underline-offset-4 decoration-secondary-gray'>
+                    <SyntaxHighlighter language="javascript" style={atomOneDark} customStyle={
+                        {
+                            backgroundColor: "#011627",
+                            padding: "0"
+                        }
+                    }>
+                        {`const githubLink = "https://github.com/example/url"`}
+                    </SyntaxHighlighter>
+                </Link>
+
             </div>
         </div>
     )
