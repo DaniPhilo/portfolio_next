ce27078d3d0e3f9c1a10f08347143ec28fd99551
DaniPhilo <daniel.carrasco.casado@gmail.com>
2023-08-28 19:11:13 +0200

    Add project card transitions and syles

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,6 +1,10 @@
 "use client"
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react"
 import { Project } from "@/interfaces/project"
+import Image from "next/image"
+import GhIcon from "../assets/github-icon.svg"
+import LinkIcon from "../assets/external-link.svg"
+import Link from "next/link"
 
 interface ProjectCardProps {
     project: Project
@@ -11,13 +15,35 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
     const [imgUrl, setImgUrl] = useState<string>("");
 
     useEffect(() => {
-      setImgUrl("bg-[url('" + project.img + "')]");
+        setImgUrl("bg-[url('" + project.img + "')]");
     }, [])
-    
+
 
     return (
-        <div className={`relative w-[500px] h-[300px] ${imgUrl} bg-cover bg-center bg-no-repeat overflow-hidden`}>
-            <h3>{project.name}</h3>
+        <div className={`group relative w-[300px] xl:w-[400px] h-[200px] xl:h-[250px] ${imgUrl} bg-cover bg-center bg-no-repeat rounded overflow-hidden duration-500 hover:scale-[1.025] `}>
+            <div className="lg:translate-y-[70%] xl:translate-y-[75%] h-full p-2 duration-500 lg:group-hover:translate-y-[0%] bg-[#01162788] lg:bg-transparent lg:bg-gradient-to-b from-transparent via-[#00000055] via-20% to-[#000000cc]">
+                <h3 className="font-semibold lg:text-xl text-highlight-text">{project.name}</h3>
+                <p className="text-accent-turquoise">{project.technologies.join(" | ")}</p>
+                <p className="text-highlight-text text-xs lg:text-sm xl:text-lg">{project.description}</p>
+            </div>
+            <div className="absolute top-0 right-0 z-10 flex gap-2 p-2">
+                <Link href={project.ghLink}>
+                    <Image
+                        src={GhIcon}
+                        alt=""
+                        width={20}
+                        height={20}
+                    />
+                </Link>
+                <Link href={project.projectLink}>
+                    <Image
+                        src={LinkIcon}
+                        alt=""
+                        width={20}
+                        height={20}
+                    />
+                </Link>
+            </div>
         </div>
     )
 }
--- helpers/projects.ts
+++ helpers/projects.ts
@@ -3,18 +3,18 @@ import { Project } from "@/interfaces/project";
 export const projects: Project[] = [
     {
         name: "Project 1",
-        technologies: [],
+        technologies: ["react", "express"],
         tags: ["react", "express"],
-        description: "",
+        description: "Una descripción un tanto escueta, pero sobretodo absurda, teniendo en cuenta que no dice absolutamente nada, ya que, como sucede a menudo, no hay nada que decir.",
         img: "https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2078&q=80",
         ghLink: "",
         projectLink: ""
     },
     {
         name: "Project 2",
-        technologies: [],
+        technologies: ["react", "sql"],
         tags: ["react", "sql"],
-        description: "",
+        description: "Si la otra descripción te ha parecido rara, esta no te va a compensar por ello. Amigo, la vida es así: un constante sorprenderse de la vacuidad de las cosas que nos cruzamos por la vida.",
         img: "https://images.unsplash.com/photo-1547082299-de196ea013d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
         ghLink: "",
         projectLink: ""
